7ef8a3b221de7edfba6c7a9d232bc5b2f48d7895
YOUNES NEMLI <y.nemli@itroadgroup.com>
2024-02-25 17:26:31 +0100

    Enhance Repositories with Optional return types and add findById methods

--- src/main/java/com/codipster/mchinacommunity/repository/RoleRepository.java
+++ src/main/java/com/codipster/mchinacommunity/repository/RoleRepository.java
@@ -4,8 +4,11 @@ import com.codipster.mchinacommunity.mongodocs.Role;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends MongoRepository<Role, String> {
 
-    Role findByRoleName(String roleName);
+    Optional<Role> findById(String id);
+    Optional<Role> findByRoleName(String roleName);
 }
--- src/main/java/com/codipster/mchinacommunity/repository/UserRepository.java
+++ src/main/java/com/codipster/mchinacommunity/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.codipster.mchinacommunity.mongodocs.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
+    Optional<User> findUserById(String id);
+    Optional<User> findByUsername(String username);
 }
