17d4fadffd8190fe4249e1791378dc917bd0a9a9
YOUNES NEMLI <y.nemli@itroadgroup.com>
2024-02-25 03:47:19 +0100

    Setup the project

---
+++ src/main/java/com/codipster/mchinacommunity/MchinaCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.codipster.mchinacommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MchinaCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MchinaCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codipster/mchinacommunity/MchinaCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codipster.mchinacommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MchinaCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
